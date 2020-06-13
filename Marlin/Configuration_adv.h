467500c150d5d1e5eea677c3c9052339af30dc76
Diaconu Cosmin <bcdiaconu@gmail.com>
2020-06-13 19:51:02 +0300

    added extruder fan to config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -420,12 +420,12 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN -1
-#define E1_AUTO_FAN_PIN -1
-#define E2_AUTO_FAN_PIN -1
-#define E3_AUTO_FAN_PIN -1
-#define E4_AUTO_FAN_PIN -1
-#define E5_AUTO_FAN_PIN -1
+#define E0_AUTO_FAN_PIN ZRIB_V20_D6_PIN
+#define E1_AUTO_FAN_PIN ZRIB_V20_D6_PIN
+#define E2_AUTO_FAN_PIN ZRIB_V20_D6_PIN
+#define E3_AUTO_FAN_PIN ZRIB_V20_D6_PIN
+#define E4_AUTO_FAN_PIN ZRIB_V20_D6_PIN
+#define E5_AUTO_FAN_PIN ZRIB_V20_D6_PIN
 #define CHAMBER_AUTO_FAN_PIN -1
 
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 60
