26942a7b7f0787ac3e57eedd8e6b0ef4b4794da8
Diaconu Cosmin <bcdiaconu@gmail.com>
2020-06-17 17:48:24 +0300

    added more defines

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 150, 100, 8, 50 }
+#define DEFAULT_MAX_FEEDRATE          { ZONESTAR_P802QS_FEEDRATE_X, ZONESTAR_P802QS_FEEDRATE_Y, ZONESTAR_P802QS_FEEDRATE_Z, ZONESTAR_P802QS_FEEDRATE_E }
 
 #define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
