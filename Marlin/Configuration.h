f045e071676b25e5b44ef78209b29b1d2b9c1c3c
Diaconu Cosmin <bcdiaconu@gmail.com>
2020-06-18 21:27:14 +0300

    changed filament load-unload lengths

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -572,7 +572,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 600
+#define EXTRUDE_MAXLENGTH 800
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
