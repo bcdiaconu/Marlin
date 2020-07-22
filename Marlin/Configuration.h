34883219c1c1200fc90963b3925f87827bee3177
Diaconu Cosmin <bcdiaconu@gmail.com>
2020-07-22 15:10:59 +0300

    changed LEVEL_CORNERS_HEIGHT to 0.1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1354,7 +1354,7 @@
 
 #if ENABLED(LEVEL_BED_CORNERS)
   #define LEVEL_CORNERS_INSET_LFRB { ZONESTAR_P802QS_PROBE_INSET, ZONESTAR_P802QS_PROBE_INSET, ZONESTAR_P802QS_PROBE_INSET, ZONESTAR_P802QS_PROBE_INSET } // (mm) Left, Front, Right, Back insets
-  #define LEVEL_CORNERS_HEIGHT        1   // (mm) Z height of nozzle at leveling points
+  #define LEVEL_CORNERS_HEIGHT      0.1   // (mm) Z height of nozzle at leveling points
   #define LEVEL_CORNERS_Z_HOP       2.0   // (mm) Z height of nozzle between leveling points
   //#define LEVEL_CENTER_TOO              // Move to the center after the last corner
 #endif
